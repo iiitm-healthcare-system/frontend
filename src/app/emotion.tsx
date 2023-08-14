6de1037e740b8cdf59c7b2e1c2549cc388565505
Raj <varasaniraj5@gmail.com>
2023-08-14 16:43:10 +0530

    fix issue in font

--- src/app/emotion.tsx
+++ src/app/emotion.tsx
@@ -3,9 +3,10 @@
 import { CacheProvider } from "@emotion/react";
 import { useEmotionCache, MantineProvider } from "@mantine/core";
 import { useServerInsertedHTML } from "next/navigation";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import config from "@/utils/config";
 import { Manrope } from "next/font/google";
-const manropeFont = Manrope({ subsets: ["latin"] });
+const manropeFont = Manrope({ subsets: ["latin"], fallback: ["Manrope"] });
 
 export default function RootStyleRegistry({
   children,
@@ -25,78 +26,80 @@ export default function RootStyleRegistry({
   ));
 
   return (
-    <CacheProvider value={cache}>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Mantine theme override here */
-          colorScheme: "light",
-          colors: {
-            primary: [
-              "#F1F3F5",
-              "#CED8E0",
-              "#A9BFD4",
-              "#80AAD1",
-              "#4F98DC",
-              "#0F89F9",
-              "#2676BF",
-              "#356692",
-              "#3B5873",
-              "#3A4C5D",
-            ],
-            secondary: [
-              "#ebf6ff",
-              "#9BB0C3",
-              "#6487A4",
-              "#283b4bb3",
-              "#364E62",
-              "#283B4B",
-              "#1C2832",
-              "#131B22",
-              "#0D1217",
-              "#090D0F",
-            ],
-            black: [
-              "#000000",
-              "#111111",
-              "#222222",
-              "#333333",
-              "#444444",
-              "#555555",
-              "#666666",
-              "#777777",
-              "#888888",
-              "#999999",
-            ],
-          },
-          primaryShade: 5,
-          primaryColor: "primary",
-          fontFamily: manropeFont.style.fontFamily,
-          defaultRadius: "var(--general-box-border-radius)",
-          headings: {
-            fontFamily: manropeFont.style.fontFamily,
-            sizes: {
-              h1: { fontSize: "var(--h1)" },
-              h2: { fontSize: "var(--h2)" },
-              h3: { fontSize: "var(--h3)" },
-              h4: { fontSize: "var(--h4)" },
-              h5: { fontSize: "var(--h5)" },
-              h6: { fontSize: "var(--h6)" },
+    <GoogleOAuthProvider clientId={config.GOOGLE_CLIENT_ID}>
+      <CacheProvider value={cache}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Mantine theme override here */
+            colorScheme: "light",
+            colors: {
+              primary: [
+                "#F1F3F5",
+                "#CED8E0",
+                "#A9BFD4",
+                "#80AAD1",
+                "#4F98DC",
+                "#0F89F9",
+                "#2676BF",
+                "#356692",
+                "#3B5873",
+                "#3A4C5D",
+              ],
+              secondary: [
+                "#ebf6ff",
+                "#9BB0C3",
+                "#6487A4",
+                "#283b4bb3",
+                "#364E62",
+                "#283B4B",
+                "#1C2832",
+                "#131B22",
+                "#0D1217",
+                "#090D0F",
+              ],
+              black: [
+                "#000000",
+                "#111111",
+                "#222222",
+                "#333333",
+                "#444444",
+                "#555555",
+                "#666666",
+                "#777777",
+                "#888888",
+                "#999999",
+              ],
             },
-          },
-          fontSizes: {
-            // xs: "0.625rem",
-            // sm: "0.875rem",
-            // md: "1.5rem",
-            // lg: "1.125rem",
-            // xl: "1.25rem",
-            // xxl: "1.5rem",
-          },
-        }}
-      >
-        {children}
-      </MantineProvider>
-    </CacheProvider>
+            primaryShade: 5,
+            primaryColor: "primary",
+            fontFamily: manropeFont.style.fontFamily,
+            defaultRadius: "var(--general-box-border-radius)",
+            headings: {
+              fontFamily: manropeFont.style.fontFamily,
+              sizes: {
+                h1: { fontSize: "var(--h1)" },
+                h2: { fontSize: "var(--h2)" },
+                h3: { fontSize: "var(--h3)" },
+                h4: { fontSize: "var(--h4)" },
+                h5: { fontSize: "var(--h5)" },
+                h6: { fontSize: "var(--h6)" },
+              },
+            },
+            fontSizes: {
+              // xs: "0.625rem",
+              // sm: "0.875rem",
+              // md: "1.5rem",
+              // lg: "1.125rem",
+              // xl: "1.25rem",
+              // xxl: "1.5rem",
+            },
+          }}
+        >
+          {children}
+        </MantineProvider>
+      </CacheProvider>{" "}
+    </GoogleOAuthProvider>
   );
 }
---
+++ src/components/home/LoginModal.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button, Title, Text, Anchor } from "@/components/mantine.helper";
+
+import LogoIIITM from "@/assets/logo_iiitm.png";
+import HeroCover from "@/assets/home/hero_cover.svg";
+import FeaturePersonalLevelMedicalRecords from "@/assets/home/features/personal_level_medical_records.svg";
+import FeaturePrescriptionMails from "@/assets/home/features/prescription_mails.svg";
+import FeatureCompleteUsageStats from "@/assets/home/features/complete_usage_stats.svg";
+
+import styles from "./Hero.module.css";
+
+const features = [
+  {
+    title: ["Personal Level", "Medical Records"],
+    icon: FeaturePersonalLevelMedicalRecords,
+    color: "#FBD300",
+    background: "#FFFCEA",
+  },
+  {
+    title: ["Prescription", "Mails"],
+    icon: FeaturePrescriptionMails,
+    color: "#11D100",
+    background: "#DBFFD8",
+  },
+  {
+    title: ["Complete", "Usage Stats"],
+    icon: FeatureCompleteUsageStats,
+    color: "#2087FF",
+    background: "#E5F1FF",
+  },
+];
+
+function LoginModal() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <Image src={LogoIIITM} alt="IIITM Gwalior" />
+        <div className={styles.headerRight}>
+          <Button
+            component={"a"}
+            href={"https://github.com/RajVarsani/btp-frontend"}
+            target={"_blank"}
+            size="md"
+            variant="subtle"
+            color="secondary"
+          >
+            GitHub
+          </Button>
+          <Button component={Link} href={"/login"} size="md">
+            Login
+          </Button>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.mainLeft}>
+          <div className={styles.mainLeftText}>
+            <Title order={1}>
+              Step Towards Accessibility and Transparency in Healthcare
+            </Title>
+            <Text
+              size={"md"}
+              color="black.7"
+              className={styles.mainLeftTextDesc}
+            >
+              Aimed at enhancing your healthcare experience and providing access
+              and manage all your medical information at IIITM Health Center in
+              one place
+            </Text>
+          </div>
+          <div className={styles.features}>
+            {features.map((feature, index) => (
+              <div
+                key={index}
+                className={styles.feature}
+                style={{ background: feature.background }}
+              >
+                <div className={styles.featureIcon}>
+                  <Image src={feature.icon} alt={feature.title.join(" ")} />
+                </div>
+                <div className={styles.featureText}>
+                  <Title order={5} style={{ color: feature.color }}>
+                    {feature.title.map((title, index) => (
+                      <Text inherit key={index}>
+                        {title}
+                      </Text>
+                    ))}
+                  </Title>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.mainRight}>
+          <Image src={HeroCover} alt="Healthcare" fill />
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <Text color="black.5" align="center" size="md">
+          Deveoped with ♥️ at{" "}
+          <Anchor href="https://www.iiitm.ac.in/" target="_blank">
+            IIITM Gwalior
+          </Anchor>
+        </Text>
+      </footer>
+    </div>
+  );
+}
+
+export default LoginModal;
